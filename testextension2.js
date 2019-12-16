150b3e3c8c15fe68feb1aefa5c9686475bb03862
bhoudou <58117370+bhoudou@users.noreply.github.com>
2019-12-16 09:50:51 +0100

    Update testextension2.js

--- testextension2.js
+++ testextension2.js
@@ -1,29 +1,39 @@
 (function(ext) {
   var IDData = null;
+  var texterreur="no error";
+
   function updateIDLocation() {
+
+
     $.ajax({
       type: "GET",
       dataType: "json",
-      url: "https://bhoudou.github.io/post_get_scratch/positions.json",
-      success: function(data) {
-        IDData = data;
-      },
-      error: function(jqxhr, textStatus, error) {
-        console.log("Error downloading ID data");
-      }
+      crossDomain: true,
+
+     url: "https://bhoudou.github.io/post_get_scratch/data.json",
+      success: function(data) {console.log(data);IDData = data;},
+      error: function(data, textStatus, errorThrown) { 
+	     IDData = 9999;
+	     console.log(errorThrown);
+	     alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+             texterreur="Error : "+textStatus+" type : " + errorThrown.responseText;      } 
     });
   }
 
-  ext.getIDInfo = function(stat) {
+  ext.getIDInfo = function(id,info) {
     if (!IDData) return;
-    if (stat === "longitude" || stat === "latitude")
-      return IDData[stat].toFixed(6).toString();
-    else
-      return IDData[stat].toFixed(2).toString();
+	  {
+		  var temp = IDData["data"];
+		  for (var i=0; i < temp.length; i++) {
+			var temp2 = temp[i];
+			if(id==temp2["id"]) return temp2[info].toFixed(1).toString();
+			}
+	  }
   };
 
   ext._getStatus = function() {
-    return { status:2, msg:'Ready' };
+    if (IDData==9999) return { status:1, msg:texterreur };
+	  return { status:2, msg:'Ready' };
   };
 
   ext._shutdown = function() {
@@ -33,19 +43,36 @@
     }
   };
 
-  var descriptor = {
-    blocks: [
-      ['r', 'current ID %m.loc', 'getIDInfo', 'x']
-    ],
-    menus: {
-      loc: ['x', 'y', 'angle']
-    },
-  };
+ ext.set_gpio = function(moteur,valeur) {
+        $.ajax({
+              url: 'http://192.168.1.43/gpio?gauche='+moteur+'&droite='+valeur+'&token=123abCde',
+              type : 'POST',
+	        });
+    };
+ ext.set_gpio2 = function(gauche,droite) {
+        $.ajax({
+              url: 'http://192.168.1.43/gpio?gauche='+gauche+'&droite='+droite+'&token=123abCde',
+              type : 'POST',
+	        });
+    };
 
-  ScratchExtensions.register('ID Tracker', descriptor, ext);
-
+// Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+		[' ', 'Moteur : %m.moteur valeur : %n','set_gpio','gauche','89'],
+		[' ', 'gauche : %n droite : %n','set_gpio2','89','89'],
+		['r', 'ID: %n valeur: %m.loc', 'getIDInfo','15', 'x'],
+        ],
+	menus: {
+       		moteur: ['gauche', 'droite'],
+		loc: ['x', 'y', 'angle']
+	},
+    };
+    // Register the extension
+    ScratchExtensions.register('GPIOESP8266', descriptor, ext);
   updateIDLocation();
   var poller = setInterval(updateIDLocation, 2000);
 
 })({});
+
 
