2954ef9d7b57d4fcf7b1cc6236414abfc1c7e119
bhoudou <58117370+bhoudou@users.noreply.github.com>
2019-12-16 08:29:42 +0100

    Update testextension.js

--- testextension.js
+++ testextension.js
@@ -4,7 +4,7 @@
     $.ajax({
       type: "GET",
       dataType: "json",
-      url: "http://192.168.1.45/data.json",
+      url: 'http://192.168.1.45/data.json',
       //url: "smb://192.168.1.45/partage_public/data.json",
       //url: "https://bhoudou.github.io/post_get_scratch/data.json",
       success: function(data) {
