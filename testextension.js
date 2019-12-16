5150eab4e656df9c630cc7dd795df7c69205a288
bhoudou <58117370+bhoudou@users.noreply.github.com>
2019-12-16 21:05:19 +0100

    Update testextension.js

--- testextension.js
+++ testextension.js
@@ -1,7 +1,10 @@
 (function(ext) {
   var IDData = null;
   var texterreur="no error";
-
+$.getJSON("http://192.168.1.45/data.json?callback=?", function(result){
+   //response data are now in the result variable
+   alert(result);
+});
   function updateIDLocation() {
     $.ajax({
       type: "GET",
@@ -70,8 +73,8 @@
     };
     // Register the extension
     ScratchExtensions.register('GPIOESP8266', descriptor, ext);
-  updateIDLocation();
-  var poller = setInterval(updateIDLocation, 2000);
+  //updateIDLocation();
+  //var poller = setInterval(updateIDLocation, 2000);
 
 })({});
 
